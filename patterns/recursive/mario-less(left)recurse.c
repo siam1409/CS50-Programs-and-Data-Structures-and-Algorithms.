e77376dfc0cd625b14c61d2f3170498c61225203
siam1409 <80522382+siam1409@users.noreply.github.com>
2021-06-27 18:57:32 +0600

    mario left aligned less - recursive --> done

---
+++ patterns/recursive/mario-less(left)recurse.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void build(int n, int x, int y);
+
+int main(void)
+{
+    int hight;
+    do
+    {
+    hight = get_int("Enter the hight: ");
+    }
+    while (hight < 0);
+
+    build(hight, 1, 0);
+}
+
+
+// the 'x' variable keeps track of rows and the 'y' variable keeps track of number of hashes in each row
+void build(int n, int x, int y)
+{
+    if (x == n + 1)
+    {
+        return;
+    }
+
+    printf("#");
+    y++;
+
+    if (y == x)
+    {
+        printf("\n");
+        build(n, x + 1, 0);
+    }
+    else
+    {
+        build(n, x, y);
+    }
+}
