c10619c0385a5cff1e3995bf0c8c5c37c6493f6b
siam1409 <80522382+siam1409@users.noreply.github.com>
2021-07-02 20:30:50 +0600

    done with right aligned pyramid

--- patterns/recursive/mario-less(right)recurse.c
+++ patterns/recursive/mario-less(right)recurse.c
@@ -1,1 +1,54 @@
 
+#include <stdio.h>
+
+void build(int n, int x, int s, int y);
+
+int main(void)
+{
+    int hight;
+
+    printf("Enter the hight: ");
+
+    scanf("%d", &hight);
+
+    build(hight, 1, 0, 0);
+
+    return 0;
+}
+
+
+void build(int n, int x, int s, int y)
+{
+    if (n == y)
+    {
+        printf("\n");
+        return;
+    }
+    if (y == x)
+    {
+        printf("\n");
+        return build(n, x + 1, 0, 0);
+    }
+
+    if (y == 0 && x < n)
+    {
+        printf(" ");
+        s++;
+    }
+
+    if (s == (n - 1) - (x - 1))
+    {
+        printf("#");
+        y++;
+        return build(n, x, s, y);
+    }
+    else
+    {
+        return build(n, x, s, y);
+    }
+}
+
+
+
+
+
