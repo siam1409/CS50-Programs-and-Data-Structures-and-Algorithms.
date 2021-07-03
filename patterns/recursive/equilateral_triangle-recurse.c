8fd9b26b9d5b2aef9cc10ba799b0df9b5a5cfbb8
siam1409 <80522382+siam1409@users.noreply.github.com>
2021-07-03 16:09:23 +0600

    done with recursive equilateral

---
+++ patterns/recursive/equilateral_triangle-recurse.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void build(int n, int x, int s, int y);
+
+int main(void)
+{
+    int hight;
+
+    printf("Enter the hight: ");
+
+    scanf("%d", &hight);
+
+    build(hight, 1, 0, 0);
+
+    return 0;
+}
+
+
+void build(int n, int x, int s, int y)
+{
+    if (y == (2 * n) - 1)
+    {
+        printf("\n");
+        return;
+    }
+    if (y == (2 * x) - 1)
+    {
+        printf("\n");
+        return build(n, x + 1, 0, 0);
+    }
+
+    if (y == 0 && x < n)
+    {
+        printf(" ");
+        s++;
+    }
+
+    if (s == (n - 1) - (x - 1))
+    {
+        printf("*");
+        y++;
+        return build(n, x, s, y);
+    }
+    else
+    {
+        return build(n, x, s, y);
+    }
+}
+
