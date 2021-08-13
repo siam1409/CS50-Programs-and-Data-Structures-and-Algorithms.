5314be9e3f16681a4a0301799382d84d65464d70
siam1409 <80522382+siam1409@users.noreply.github.com>
2021-08-13 15:37:07 +0600

    5th flavor working strcpy file

---
+++ strcpy programs/copy5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    char *array1 = "this";
+
+    char *array2 = malloc(strlen(array1) + 1);
+
+    strcpy(array2, array1);
+
+    printf("%s\n", array2);
+
+    printf("%s\n", array1);
+
+    return 0;
+}
