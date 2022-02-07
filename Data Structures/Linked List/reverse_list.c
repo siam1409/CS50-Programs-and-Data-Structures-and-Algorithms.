4bbfe7d49512ea3e0903ca7efafc3eecb2528d6b
Abdur Rahman Siam <80522382+siam1409@users.noreply.github.com>
2022-02-07 21:13:37 +0600

    reverse a linked list

---
+++ Data Structures/Linked List/reverse_list.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "list.h"
+
+int main()
+{
+    // create the list
+    create_list();
+
+    if (number_of_nodes == 0)
+    {
+        printf("\n");
+        return 0;
+    }
+
+    // if run out of memory, exit
+    if (malloc_err)
+    {
+        printf("An error occured! :(\n");
+        return 1;
+    }
+
+    // reverse the list
+    reverse_list();
+
+    // print the list
+    print_list();
+
+    // free the list
+    free_list();
+
+    return 0;
+}
